ce4c1f2337afb5d9745e32cf4b843341e9651199
rossome89 <rossryan89@gmail.com>
2016-11-17 23:03:42 +0000

    First Commit
    Uploading work done 15/11/2016 - 17/11/2016

---
+++ LucidChords/LucidChords.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Lucid Chords</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/extras.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+	<link rel="icon" href="favicon.ico" />
+  </head>
+  <body>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script $('#column_gap_2').click(function() {
+    $('#column_gap_2').addClass('myClass');
+});
+></script>
+<!--Change id="column_gap_2" below to id="column_gap_3" to use 3 columns instead of 2-->
+	<div id="column_gap_2" class="image" height="100%" onclick="myFunction()">
+<!--Copy and paste your chords here-->
+</div>
+
+  </body>
+</html>
