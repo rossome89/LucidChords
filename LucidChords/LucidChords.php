fae3566fcc789e400613a53c7f209bcee26af168
rossome89 <rossryan89@gmail.com>
2016-12-04 00:01:36 +0000

    Updated with column toggle.

--- LucidChords/LucidChords.php
+++ LucidChords/LucidChords.php
@@ -26,14 +26,83 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-	<script $('#column_gap_2').click(function() {
-    $('#column_gap_2').addClass('myClass');
-});
-></script>
+	<script> 
+function myFunction()
+{
+//    document.getElementById('result').setAttribute("class", "column_gap_3");
+
+if(document.getElementById('result').className.match('column_gap_2')) {
+            document.getElementById('result').setAttribute("class", "column_gap_3");
+        }
+        else {
+            document.getElementById('result').setAttribute("class", "column_gap_2");
+        }
+}
+</script>
+<script> // Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}</script>
+<output id="list"></output>
 <!--Change id="column_gap_2" below to id="column_gap_3" to use 3 columns instead of 2-->
-	<div id="column_gap_2" class="image" height="100%" onclick="myFunction()">
-<!--Copy and paste your chords here-->
+	<div class="column_gap_2" id="result" height="100%" onclick="myFunction()">
 </div>
+
+<script>
+window.onload = function() {
+
+    //Check File API support
+    if (window.File && window.FileList && window.FileReader) {
+        var filesInput = document.getElementById("files");
+
+        filesInput.addEventListener("change", function(event) {
+
+            var files = event.target.files; //FileList object
+            var output = document.getElementById("result");
+
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+
+                //Only plain text
+                if (!file.type.match('plain')) continue;
+
+                var picReader = new FileReader();
+
+                picReader.addEventListener("load", function(event) {
+
+                    var textFile = event.target;
+
+                    var div = document.createElement("div");
+
+                    div.innerText = textFile.result;
+
+                    output.insertBefore(div, null);
+
+                });
+
+                //Read the text file
+                picReader.readAsText(file);
+				$("#hidden-div").toggle()
+            }
+
+        });
+    }
+    else {
+        console.log("Your browser does not support File API");
+    }
+}
+</script>
+
+
+
+
+<article id="hidden-div">
+        <label for="files">Select a file: </label>
+        <input id="files" type="file" class="form-control" />
+        <output  />
+    </article>
 
   </body>
 </html>
